c5b9daf2c7c23ac055b3962720f1a71e407b5e5b
Nipun Liyanage <51255650+NipunLiyanage@users.noreply.github.com>
2022-02-14 12:05:47 +0530

    Update dashboard.java

--- app/src/main/java/com/example/cardoctor/dashboard.java
+++ app/src/main/java/com/example/cardoctor/dashboard.java
@@ -5,8 +5,9 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.view.view;
 import android.os.Bundle;
-import android.view.View;
+
 
 public class dashboard extends AppCompatActivity {
 
